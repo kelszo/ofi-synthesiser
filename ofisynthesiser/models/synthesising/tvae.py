888764b30eb6b66406ea6cc8d203b6097c165cda
Kelvin John Falk Szolnoky <me@kelszo.com>
2023-02-07 17:05:15 +0100

    [models] generate atleast as many ofi cases as input

--- ofisynthesiser/models/synthesising/tvae.py
+++ ofisynthesiser/models/synthesising/tvae.py
@@ -16,8 +16,13 @@ def generate_data_tvae(data: pd.DataFrame, cuda: bool, debug: bool = False) -> p
 
     df_synth = model.sample(num_rows=len(data))
 
-    if len(df_synth["ofi"].value_counts()) == 1:
-        condition = sdv.sampling.Condition({"ofi": 1}, num_rows=data["ofi"].value_counts()[1])
+    if len(df_synth["ofi"].value_counts()) == 1 or df_synth["ofi"].value_counts()[1] < data["ofi"].value_counts()[1]:
+        n_rows_to_generate = data["ofi"].value_counts()[1]
+
+        if len(df_synth["ofi"].value_counts()) == 2:
+            n_rows_to_generate = data["ofi"].value_counts()[1] - df_synth["ofi"].value_counts()[1]
+
+        condition = sdv.sampling.Condition({"ofi": 1}, num_rows=n_rows_to_generate)
         df_ofi_synth = model.sample_conditions(conditions=[condition])
 
         df_synth = pd.concat([df_synth, df_ofi_synth])
